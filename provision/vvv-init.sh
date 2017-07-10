f4c4f236a0e6534e885f3b01cce19129da92aba2
Sauli Rajala <sauli.rajala@valu.fi>
2017-07-10 13:06:51 +0300

    Add .dev to wp core install --url

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -36,7 +36,7 @@ define( 'WP_DEBUG', true );
 PHP
 
   echo "Installing WordPress Stable..."
-  noroot wp core install --url=${VVV_SITE_NAME} --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+  noroot wp core install --url=${VVV_SITE_NAME}.dev --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
 
 else
 
